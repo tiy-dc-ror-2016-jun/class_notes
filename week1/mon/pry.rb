fbbb6b7574230b449c270c3a463e051046243586
Russell Osborne <russell@burningpony.com>
2016-06-20 14:06:00 -0400

    Adding pry history day 1

---
+++ week1/mon/pry.rb
@@ -0,0 +1,32 @@
+word = "racecar"
+ls word
+word
+word.class
+word.methods
+word.itself
+ls word
+word.reverse
+word = "joe"
+word.reverse
+word == word.reverse
+
+1
+1.class
+1
+a = 1
+a
+ls a
+a.to_s
+1.0
+1 / 3
+2 / 3
+2 / 3.0
+ls 2
+9.to_s(2)
+2.to_s(2)
+3 /1
+2 / 3
+2 / 5.0
+2 / 7.0
+129.95 * 100
+0.28 * 100
