ce1dbef29d0bbfb2798a4006bff7b066feb26965
Russell Osborne <russell@burningpony.com>
2016-07-19 22:25:03 -0400

    adding bug fix for doctype / css parsing

--- week5/tuesday/app.rb
+++ week5/tuesday/app.rb
@@ -4,18 +4,19 @@ class MyFavoriteWeatherAppEver < Sinatra::Base
 
   get "/" do
     zip_code = params["zip"]
-
+    weather = ""
     ERB.new(File.read("./views/homepage.html.erb")).result(binding)
   end
 
 
   get "/cool.css" do
+    content_type "text/css"
     File.read("./assets/css/styles.css")
   end
 
   post "/get_weather" do
     zip_code = params["zip"]
-    
+
     weather = WeatherClient.new(zip_code).return_current_weather
 
     ERB.new(File.read("./views/homepage.html.erb")).result(binding)
