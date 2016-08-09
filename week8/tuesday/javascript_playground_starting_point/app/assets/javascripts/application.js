7c949e294c2bababe690a0afe8d82e34ae7df301
Russell Osborne <russell@burningpony.com>
2016-08-09 12:21:27 -0400

    Adding notes from in class

--- week8/tuesday/javascript_playground_starting_point/app/assets/javascripts/application.js
+++ week8/tuesday/javascript_playground_starting_point/app/assets/javascripts/application.js
@@ -14,65 +14,124 @@
 //= require jquery_ujs
 //= require_tree .
 
-var nameLink = document.getElementById('name_link');
-nameLink.addEventListener("click", function () {
-  var name_field = document.getElementById('name_field');
-  name_field.value = "Backflip";
-});
+console.log(1+1);
 
-var trueRadioBtn = document.getElementById('show_true')
-trueRadioBtn.addEventListener("click", function () {
-  var section = document.getElementById('show_hide_section');
-  section.style = "display: block";
-});
+var list_elements = document.getElementsByClassName("list-element");
+console.log(list_elements);
 
-var copyLink = document.getElementById('copy_link')
+for (var i = 0; i < list_elements.length; i++) {
+  var list_element = list_elements[i];
+  list_element.style.color = "red";
+}
 
-copyLink.addEventListener("click", function () {
-  var copySection = document.getElementById('copy_section');
-  
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//alert("I HAVE YOU NOW!");
+// Core JS
+// // Define Callback
+// function toggleSection(event) {
+//   console.log(event);
+//   var el = document.getElementById('show_hide_section');
+//   el.style.display = "block";
+// };
 //
-// function defaultName() {
-//   var nameField = document.getElementById("name_field");
-//   nameField.value = "Emperor Goatimus";
-// }
+// // Find and bind event
+// var toggle_show = document.getElementById("show_true");
+// toggle_show.addEventListener("click", toggleSection)
 //
-// function displaySection(displayType) {
-//   var section = document.getElementById("show_hide_section");
-//   section.style.display = displayType;
-// }
+
+function toggleSection(event) {
+  var el = $('#show_hide_section');
+
+  if (el.is(":visible")) {
+    el.hide();
+  } else {
+    el.show();
+  }
+}
+
+$("#show_true").on("click", toggleSection)
+$("#show_false").on("click", toggleSection)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var nameLink = document.getElementById('name_link');
+// nameLink.addEventListener("click", function () {
+//   var name_field = document.getElementById('name_field');
+//   name_field.value = "Backflip";
+// });
 //
-// function copySection() {
-//   var section = document.getElementById("copy_section");
-//   var copy = section.lastElementChild.cloneNode(true);
-//   section.appendChild(copy);
-// }
+// var trueRadioBtn = document.getElementById('show_true')
+// trueRadioBtn.addEventListener("click", function () {
+//   var section = document.getElementById('show_hide_section');
+//   section.style = "display: block";
+// });
+//
+// var copyLink = document.getElementById('copy_link')
+//
+// copyLink.addEventListener("click", function () {
+//   var copySection = document.getElementById('copy_section');
+//
+// });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// //alert("I HAVE YOU NOW!");
+// //
+// // function defaultName() {
+// //   var nameField = document.getElementById("name_field");
+// //   nameField.value = "Emperor Goatimus";
+// // }
+// //
+// // function displaySection(displayType) {
+// //   var section = document.getElementById("show_hide_section");
+// //   section.style.display = displayType;
+// // }
+// //
+// // function copySection() {
+// //   var section = document.getElementById("copy_section");
+// //   var copy = section.lastElementChild.cloneNode(true);
+// //   section.appendChild(copy);
+// // }
