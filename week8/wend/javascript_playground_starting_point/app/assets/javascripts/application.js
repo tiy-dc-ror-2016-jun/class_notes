52e4ee9731569e7a21003f4a8180ac5042dcbc38
Russell Osborne <russell@burningpony.com>
2016-08-10 11:29:43 -0400

    Add clone and jquery notes

--- week8/wend/javascript_playground_starting_point/app/assets/javascripts/application.js
+++ week8/wend/javascript_playground_starting_point/app/assets/javascripts/application.js
@@ -14,124 +14,56 @@
 //= require jquery_ujs
 //= require_tree .
 
-console.log(1+1);
+$(document).on("ready", doStuff)
 
-var list_elements = document.getElementsByClassName("list-element");
-console.log(list_elements);
+function doStuff() {
+  console.log(1+1);
 
-for (var i = 0; i < list_elements.length; i++) {
-  var list_element = list_elements[i];
-  list_element.style.color = "red";
-}
+  var list_elements = document.getElementsByClassName("list-element");
+  console.log(list_elements);
 
-// Core JS
-// // Define Callback
-// function toggleSection(event) {
-//   console.log(event);
-//   var el = document.getElementById('show_hide_section');
-//   el.style.display = "block";
-// };
-//
-// // Find and bind event
-// var toggle_show = document.getElementById("show_true");
-// toggle_show.addEventListener("click", toggleSection)
-//
-
-function toggleSection(event) {
-  var el = $('#show_hide_section');
-
-  if (el.is(":visible")) {
-    el.hide();
-  } else {
-    el.show();
+  for (var i = 0; i < list_elements.length; i++) {
+    var list_element = list_elements[i];
+    list_element.style.color = "red";
   }
+
+  // Core JS
+  // // Define Callback
+  // function toggleSection(event) {
+  //   console.log(event);
+  //   var el = document.getElementById('show_hide_section');
+  //   el.style.display = "block";
+  // };
+  //
+  // // Find and bind event
+  // var toggle_show = document.getElementById("show_true");
+  // toggle_show.addEventListener("click", toggleSection)
+  //
+
+  function toggleSection(event) {
+    var el = $('#show_hide_section');
+    if (el.is(":visible")) {
+      el.hide();
+    } else {
+      el.show();
+    }
+  }
+
+  $("#show_true").on("click", toggleSection)
+  $("#show_false").on("click", toggleSection)
+
+
+  function copySelection(event) {
+    // event.preventDefault();
+    // console.log(event);
+    var el = $("#copy_section").children();
+    $("#copy_section").append(el.clone());
+
+    var el = $("#copy_link");
+    $("#copy_section").append(el.clone());
+  }
+  // copySelection();
+
+  $("#copy_link").on("click", copySelection)
+
 }
-
-$("#show_true").on("click", toggleSection)
-$("#show_false").on("click", toggleSection)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var nameLink = document.getElementById('name_link');
-// nameLink.addEventListener("click", function () {
-//   var name_field = document.getElementById('name_field');
-//   name_field.value = "Backflip";
-// });
-//
-// var trueRadioBtn = document.getElementById('show_true')
-// trueRadioBtn.addEventListener("click", function () {
-//   var section = document.getElementById('show_hide_section');
-//   section.style = "display: block";
-// });
-//
-// var copyLink = document.getElementById('copy_link')
-//
-// copyLink.addEventListener("click", function () {
-//   var copySection = document.getElementById('copy_section');
-//
-// });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// //alert("I HAVE YOU NOW!");
-// //
-// // function defaultName() {
-// //   var nameField = document.getElementById("name_field");
-// //   nameField.value = "Emperor Goatimus";
-// // }
-// //
-// // function displaySection(displayType) {
-// //   var section = document.getElementById("show_hide_section");
-// //   section.style.display = displayType;
-// // }
-// //
-// // function copySection() {
-// //   var section = document.getElementById("copy_section");
-// //   var copy = section.lastElementChild.cloneNode(true);
-// //   section.appendChild(copy);
-// // }
